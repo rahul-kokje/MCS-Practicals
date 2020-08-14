7f82ecb178cdece15af6721ab6803c84a3e0f8ee
Rahul Kokje <59117489+rahul-kokje@users.noreply.github.com>
2020-08-14 22:56:55 +0530

    Create pract_1.py
    Write a python program to Prepare Scatter Plot (Use Forge Dataset / Iris Dataset)

---
+++ ML/pract_1.py
@@ -0,0 +1,22 @@
+#Write a python program to Prepare Scatter Plot (Use Forge Dataset / Iris Dataset)
+
+data = sb.load_dataset('iris')
+print(data)
+
+fig = mp.figure()
+
+#Comment out one of the following..
+fig = fog.add_axes([0, 0, 1, 1])    #use it for jupyter notebook
+fig = fig.add_subplot(111)          #use it for command line
+
+fig.set_xlabel('Species')
+fig.set_ylabel('Petal Width')
+
+x = data['species']
+y1 = data['petal_width']
+y2 = data['sepal_width']
+
+fig.scatter(x, y1, c='m', marker='+', s=100)
+fig.scatter(x, y2, c='c', s=100)
+
+mp.show()
